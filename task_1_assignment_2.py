add8a0804084bfef1c3f74a7a2fae3d2065c7612
Gaurav Pilania <gpilania9@gmail.com>
2025-06-25 15:16:31 +0530

    Add files via upload

---
+++ task_1_assignment_2.py
@@ -0,0 +1,10 @@
+#ASSIGNMENT 2 (TASK-1): Check if a Number is Even or Odd
+
+num=int(input("Enter a number: "))
+
+# checking and displaying
+
+if (num%2==0):
+    print(num," is an even number.")
+else:
+    print(num," is an odd number.")
