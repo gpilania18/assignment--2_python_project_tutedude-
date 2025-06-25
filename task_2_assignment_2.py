85d6e75e07a8edcc50b0ce48d2cb1aacbd4e879e
Gaurav Pilania <gpilania9@gmail.com>
2025-06-25 15:32:34 +0530

    Add files via upload

---
+++ task_2_assignment_2.py
@@ -0,0 +1,9 @@
+#ASSIGNMENT 2 (TASK-2): Sum of Integers from 1 to 50 Using a Loop
+sum =0
+#using for loop to add to variable sum
+for i in range (1,51):
+    sum+=i
+
+# displaying the result
+print("The sum of numbers from 1 to 50 is:",sum)
+
